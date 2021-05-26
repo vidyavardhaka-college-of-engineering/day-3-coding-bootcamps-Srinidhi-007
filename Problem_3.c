8a4d7992bbe75448b2c865b1194c1ebe2344a6a1
Srinidhi007 <>
2021-05-26 12:09:25 +0000

    I did the coding

--- Problem_3.c
+++ Problem_3.c
@@ -1,13 +1,17 @@
 // Check whether an integer is odd or even
 
 #include <stdio.h>
-int main() {
+int main() 
+{
     int number;
     printf("Enter an integer: ");
     scanf("%d", &number);
 
     // Write your code here
-    
+    if(number%2==0)
+    printf("Even");
+    else
+    printf("Odd");
     
    
     return 0;
